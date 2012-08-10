ff357072b89f5f9c52da15c75c319d705f1a2cd3
Roberto <topikito@gmail.com>
2012-08-10 13:29:02 +0200

    [D] Silex repo as you had to "compile" the package in every update [N] Silex repo as a folder

--- assets/js/codazo-vitamins.js
+++ assets/js/codazo-vitamins.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-
 	$(function() {
 		var availableTags = ['ActionScript','AppleScript','Asp','BASIC','C','C++','Clojure','COBOL','ColdFusion','Erlang','Fortran','Groovy','Haskell','Java','JavaScript','Lisp','Perl','PHP','Python','Ruby','Scala','Scheme'];
 		$( '#lang' ).autocomplete({
 			source: availableTags
 		});
 	});
-
 	$(function() {
 		$('#code').keyup(function() {
 			alert('asdad');
@@ -14,6 +12,4 @@ $(document).ready(function() {
 			$('#pretty-code').show().html(code);
 		});
 	});
-
-
 });
